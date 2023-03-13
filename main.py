895e2d97dbc20b7a773db0c3476f794f0052200e
Jürgen Eckel <juergen@riddleandcode.com>
2023-03-13 15:42:49 +0100

    adjustments
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import subprocess
 
 from pydantic import BaseModel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class RegisterRequest(BaseModel):
     asset_id: str
     contract: dict
 
 
-@app.post("/register_asset")
+@app.post("/register_asset", status_code=201)
 async def register_asset_id(request_body: RegisterRequest):
-    register_asset_id_local(request_body.asset_id)
+    print(f" request_body: { request_body }")
+    if register_asset_id_local(request_body.asset_id):
+        print(f"File succesffully written: {request_body.asset_id}")
     # register_asset_id_on_liquid(request_body.asset_id, request_body.contract)
-    return request_body
+    return {"asset_id": request_body.asset_id }
 
 
 def register_asset_id_local(asset_id: str):
-    asset_id_string = 'Authorize linking the domain name lab.r3c.network to the Liquid asset ' + asset_id
-    result = subprocess.run(['sudo', 'touch', '/var/www/html/.well-known/liquid-asset-proof-' + asset_id],
-                            stdout=subprocess.PIPE)
-    result = subprocess.run(['echo', '/var/www/html/.well-known/'], stdout=subprocess.PIPE)
-
-    ps = subprocess.run(['echo', asset_id_string], check=True, capture_output=True)
-    processNames = subprocess.run(['sudo', 'tee', '-a', '/var/www/html/.well-known/liquid-asset-proof-' + asset_id],
-                                  input=ps.stdout, capture_output=True)
+    try:
+        f = open('/var/www/html/.well-known/liquid-asset-proof-' + asset_id, "w")
+        f.write('Authorize linking the domain name lab.r3c.network to the Liquid asset ' + asset_id)
+        f.close()
+    except Exception as e:
+        print(f"File Write exception: {e}")
+        return False
+    return True
 
 
 def register_asset_id_on_liquid(asset_id: str, contract: dict):
